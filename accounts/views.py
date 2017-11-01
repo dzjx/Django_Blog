91cce5164b38f441435aa422f152a34bc4bee614
dzjx <502498669@qq.com>
2017-11-01 14:48:59 +0800

    登录注册

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,70 @@
+from django.contrib import auth
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views import generic
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+
+import accounts.forms as af
+from django.contrib.auth import REDIRECT_FIELD_NAME
 
 
 # Create your views here.
 
-class LoginView(generic.RedirectView):
-    pass
+class LoginView(generic.FormView):
+    form_class = af.LoginForm
+    template_name = 'account/login.html'
+    success_url = '/'
+
+    redirect_field_name = REDIRECT_FIELD_NAME
+
+    @method_decorator(sensitive_post_parameters('password'))
+    @method_decorator(csrf_protect)
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        redirect_to = self.request.GET.get(self.redirect_field_name)
+        kwargs['redirect_to'] = redirect_to or self.success_url
+        return super(LoginView, self).get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        form = AuthenticationForm(data=self.request.POST, request=self.request)
+
+        if form.is_valid():
+            auth.login(self.request, form.get_user())
+            return super(LoginView, self).form_valid(form)
+        else:
+            return self.render_to_response({'form': form})
+
+    def get_success_url(self):
+        redirect_to = self.request.POST.get(self.redirect_field_name)
+        return redirect_to or self.success_url
 
 
 class LogoutView(generic.RedirectView):
-    pass
+    url = '/login/'
+
+    @method_decorator(never_cache)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LogoutView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        auth.logout(request)
+        return super(LogoutView, self).get(request, *args, **kwargs)
+
+
+class RegisterView(generic.FormView):
+    form_class = af.RegisterForm
+    template_name = 'account/registration_form.html'
+
+    def form_valid(self, form):
+        user = form.save(False)
+        user.save(True)
+
+        return HttpResponseRedirect(reverse('account:login'))
